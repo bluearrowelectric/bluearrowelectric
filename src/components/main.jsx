072d6722dd71afbb852294b521d0beeb4b5c4eeb
thejunglemarket <96501055+TheSpotNFT@users.noreply.github.com>
2023-11-25 21:46:54 -0800

    upldate

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -9,14 +9,14 @@ function Main() {
             {/* Your Main component content goes here */}
             <div className="text-white text-center h-full">
                 <div className='pt-12'>
-                    <div className='bg-slate-700 bg-opacity-80 pt-8 text-2xl font-serif'>
+                    <div className='bg-slate-700 bg-opacity-60 pt-8 text-2xl font-serif'>
                         Our Contracting Partners in Revelstoke BC and surrounding areas
                     </div></div>
                 <div className="h-3/5">
-                    <div className='bg-slate-700 bg-opacity-80 h-3/5'>
+                    <div className='bg-slate-700 bg-opacity-60 h-3/5'>
                         <ImageSlider className='border-4' slides={SliderData} />
                     </div><div className='pt-0'>
-                        <div className='bg-slate-700 bg-opacity-80 pt-12 text-2xl'>
+                        <div className='bg-slate-700 bg-opacity-60 pt-12 text-2xl'>
 
                         </div></div>
                 </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,16 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import logo from "../assets/images/logo.png";
-import { GiHamburgerMenu } from "react-icons/gi";
 
 export const Navbar = () => {
-
-
     let Links = [
         { name: "Home", link: "/" },
         { name: "Testimonials", link: "/testimonials" },
         { name: "Contact", link: "mailto:bluearrowelectric@gmail.com" },
-        //{ name: "Testimonials", link: "/testimonials" },
-
     ];
 
+    const location = useLocation();
     const [open, setOpen] = useState(false);
-
 
     return (
         <nav className="main-nav pb-20 pt-8 bg-transparent">
@@ -23,12 +19,11 @@ export const Navbar = () => {
                     <div className="desktop-nav">
                         <ul
                             className={`xs:flex sm:flex md:flex lg:flex xl:flex xxl:flex md:items-center lg:items-center xl:items-center xxl:items-center font-mono md:pb-0 lg:pb-0 xl:pb-0 xxl:pb-0 pb-12 absolute md:static lg:static xl:static xxl:static bg-spot-yellow md:z-auto lg:z-auto xl:z-auto xxl:z-auto z-[-1] 
-      left-0 w-full md:w-auto lg:w-auto xl:w-auto xxl:w-auto md:pl-0 lg:pl-0 xl:pl-0 xxl:pl-0 pl-9 transition-all duration-300 ease-in ${open ? "top-20 opacity-90 bg-white" : "top-[-490px]"
+                            left-0 w-full md:w-auto lg:w-auto xl:w-auto xxl:w-auto md:pl-0 lg:pl-0 xl:pl-0 xxl:pl-0 pl-9 transition-all duration-300 ease-in"
                                 }`}
                         >
                             <img src={logo} alt="logo" className="m-0 w-60 px-4 py-4"></img>
                             <div className="flex px-14 absolute right-0">
-
                                 {Links.map((link) => (
                                     <li
                                         key={link.name}
@@ -44,14 +39,9 @@ export const Navbar = () => {
                                             {link.name}
                                         </a>
                                     </li>
-                                ))}</div>
+                                ))}
+                            </div>
                         </ul>
-                    </div>
-                    <div
-                        onClick={() => setOpen(!open)}
-                        className="text-3xl absolute left-8 top-6 cursor-pointer md:hidden lg:hidden xl:hidden xxl:hidden"
-                    >
-                        <GiHamburgerMenu name={open ? "close" : "menu"} />
                     </div>
 
                 </div>
@@ -59,4 +49,3 @@ export const Navbar = () => {
         </nav>
     )
 }
-
