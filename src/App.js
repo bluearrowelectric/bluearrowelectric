49d8e1419c0dd53be3d51c5ad5e41159fe8c2dd1
thejunglemarket <96501055+TheSpotNFT@users.noreply.github.com>
2023-12-06 18:21:36 -0800

    new

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/navbar';
 import Main from './components/main';
-import Footer from './components/Footer';
-import background1 from './assets/images/BAE_BG/1.jpg';
-import background2 from './assets/images/BAE_BG/2.jpg';
-import background3 from './assets/images/BAE_BG/3.jpg';
-import background4 from './assets/images/BAE_BG/4.jpg';
-import background5 from './assets/images/BAE_BG/5.jpg';
+
 
 function App() {
-  const backgrounds = [background1, background2, background3, background4, background5];
-  const [currentBackground, setCurrentBackground] = useState('');
-
-  useEffect(() => {
-    // Select a random background image on component mount
-    const randomIndex = Math.floor(Math.random() * backgrounds.length);
-    setCurrentBackground(backgrounds[randomIndex]);
-  }, [backgrounds]);
-
   return (
     <Router>
-     <div
-        className="min-h-screen bg-cover bg-center bg-no-repeat fixed w-full transition-all duration-500"
-        style={{ backgroundImage: `url(${currentBackground})` }}
-      >
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
-        <Footer />
-      </div>
+        {/* Content */}
+        <div className="relative z-20">
+         
+          <Routes>
+            <Route path="/" element={<Main />} />
+          </Routes>
+         
+        </div>
     </Router>
   );
 }
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,33 +1,146 @@
-import React from 'react';
-import ImageSlider from './carousel';
-import { SliderData } from './carouseldata';
-
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import bg1 from '../assets/images/BAE_BG/1.jpg';
+import bg2 from '../assets/images/BAE_BG/2.jpg';
+import bg3 from '../assets/images/BAE_BG/19.jpg';
+import bg4 from '../assets/images/BAE_BG/4.jpg';
+import bg5 from '../assets/images/BAE_BG/5.jpg';
+import bg6 from '../assets/images/BAE_BG/21.jpg';
+import bg7 from '../assets/images/BAE_BG/20.jpg'
+import bg8 from '../assets/images/BAE_BG/22.jpg';
+import bg9 from '../assets/images/BAE_BG/13.jpg';
+import logo1 from '../assets/images/logo.png';
+import logo3 from '../assets/images/ParallelVision.png';
+import logo4 from '../assets/images/Mackenzie.png';
+import logo5 from '../assets/images/Appenzell.png';
 
 function Main() {
-
-
     return (
+        <div className="snap-y-manditory snap-proximity">
+            <div
+                className="min-h-screen flex justify-center items-center bg-black bg-no-repeat bg-fixed snap-center bg-cover bg-center"
+                style={{
+                    backgroundImage: `url(${bg2})`,
 
-        <div className="h-screen relative pt-24">
-            {/* Your Main component content goes here */}
-            <div className="text-white text-center h-full">
-                <div className='pt-12'>
-                    <div className='bg-slate-700 bg-opacity-60 pt-8 text-2xl font-serif'>
-                        Contact One of Our Contracting Partners in Revelstoke BC and surrounding areas to work with us on your project
-                    </div></div>
-                <div className="h-3/5">
-                    <div className='bg-slate-700 bg-opacity-60 h-3/5'>
-                        <ImageSlider className='border-4' slides={SliderData} />
-                    </div><div className='pt-0'>
-                        <div className='bg-slate-700 bg-opacity-60 pt-12 text-2xl'>
-
-                        </div></div>
+                }}
+            >
+                <div className='bg-white bg-opacity-60 w-full py-8'><img className='mx-auto' src={logo1} alt="Logo 1" /></div>
+            </div>
+            <div
+                className="min-h-screen flex justify-center items-start bg-blue bg-no-repeat bg-fixed snap-start"
+                style={{
+                    backgroundImage: `url(${bg1})`,
+                    backgroundSize: 'cover',
+                }}
+            >
+                <div className="min-h-screen flex items-start pt-96 bg-blue bg-no-repeat bg-fixed snap-start">
+                    <div className="xl:w-1/2 lg:w-2/3 md:w-4/5 sm:w-full p-8">
+                        <div className="bg-white bg-opacity-80 w-full px-16 pt-16 pb-60">
+                            <div className="text-2xl">
+                                Blue Arrow Electric Ltd has proudly served the Revelstoke, BC community for over 5 years, establishing enduring partnerships with local general contractors. As trusted subcontractors, we seamlessly integrate into the operations of various general contractors, becoming integral members of their project teams.
+                            </div>
+                            <div className="text-2xl mt-8">
+                                Our commitment to excellence is reflected in the longstanding relationships we've cultivated, grounded in shared values with our general contractor partners. At Blue Arrow Electric Ltd, we align ourselves with our contractors' core principles, emphasizing Quality, Scheduling, and Budget in every project we undertake.
+                            </div>
+                            <div className="text-2xl mt-8">
+                                We invite you to connect with one of our esteemed contracting partners today. Let's embark on the journey of planning and executing your next project together, ensuring its success through collaborative efforts and unwavering dedication.
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-
+            </div>
+            <div
+                className="min-h-screen flex justify-center items-center bg-blue bg-no-repeat bg-fixed snap-center bg-center"
+                style={{
+                    backgroundImage: `url(${bg9})`,
+                    backgroundSize: 'cover',
+                }}
+            >
 
             </div>
+            <div
+                className="min-h-screen flex justify-center items-center bg-blue bg-no-repeat bg-fixed snap-center bg-center"
+                style={{
+                    backgroundImage: `url(${bg8})`,
+                    backgroundSize: 'cover',
+                }}
+            >
 
+            </div>
+            <div
+                className="min-h-screen flex justify-center items-center bg-blue bg-no-repeat bg-fixed snap-center bg-center"
+                style={{
+                    backgroundImage: `url(${bg7})`,
+                    backgroundSize: 'cover',
+                }}
+            >
+                <a href="http://appenzellconstruction.com"><img className='w-2/3 mx-auto' src={logo5} alt="Appenzell" /></a>
+            </div>
+            <div
+                className="min-h-screen flex justify-center items-center bg-blue bg-no-repeat bg-fixed snap-center bg-center"
+                style={{
+                    backgroundImage: `url(${bg6})`,
+                    backgroundSize: 'cover',
+                }}
+            >
+
+            </div>
+            <div
+                className="min-h-screen flex justify-center items-center bg-blue bg-no-repeat bg-fixed snap-center bg-center"
+                style={{
+                    backgroundImage: `url(${bg4})`,
+                    backgroundSize: 'cover',
+                }}
+            >
+                <a href="http://parallelvisioncontracting.com"><img src={logo3} alt="Parallel Vision" /></a>
+            </div>
+            <div
+                className="min-h-screen flex justify-center items-center bg-blue bg-no-repeat bg-fixed snap-center bg-center"
+                style={{
+                    backgroundImage: `url(${bg5})`,
+                    backgroundSize: 'cover',
+                }}
+            >
+
+            </div>
+            <div
+                className="min-h-screen flex justify-center items-center bg-blue bg-no-repeat bg-fixed snap-center bg-center"
+                style={{
+                    backgroundImage: `url(${bg3})`,
+                    backgroundSize: 'cover',
+                }}
+            >
+                <a href="http://mackenziedynamic.com"><img className='w-1/3 mx-auto' src={logo4} alt="MacKenzie Dynamic" /></a>
+            </div>
+            <div
+                className="min-h-screen flex justify-center items-center bg-blue bg-no-repeat bg-fixed snap-center bg-center"
+                style={{
+                    backgroundImage: `url(${bg1})`,
+                    backgroundSize: 'cover',
+                }}
+            >
+
+            </div>
+            <div
+                className="min-h-screen flex justify-center items-center bg-blue bg-no-repeat bg-fixed snap-center bg-center"
+                style={{
+                    backgroundImage: `url(${bg7})`,
+                    backgroundSize: 'cover',
+                }}
+            >
+                <div className='bg-white bg-opacity-60 w-full py-8'><img className='mx-auto' src={logo1} alt="Logo 1" /></div>
+            </div>
+            {/*} <div
+                className="min-h-screen flex justify-center items-center bg-blue bg-no-repeat bg-fixed snap-center"
+                style={{
+                    backgroundImage: `url(${bg1})`,
+                    backgroundSize: 'cover',
+                }}
+            >
+                <img src={logo5} alt="Raven" />
+            </div>
+            */}
         </div>
     );
 }
